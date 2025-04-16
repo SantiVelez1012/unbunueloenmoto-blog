536a55b507e465e29d99565e8fc38706e184f364
Santiago Velez <santivekez1012@gmail.com>
2025-04-16 16:35:07 -0500

    trying a new way to fix the problem

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic';
+export const revalidate = 60;
 
 import BlogPostPage from "@/core/presentation/pages/blog-post/blogPostPage"
 import { Loader } from "lucide-react";
