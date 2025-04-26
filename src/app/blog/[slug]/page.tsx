a99daf7439536958ca0128a956eb50ca8fb0ef70
Santiago Velez <santivekez1012@gmail.com>
2025-04-25 20:57:05 -0500

    forcing dynamic load on Post Page ( SSR )

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-export const revalidate = 60;
+export const dynamic = 'force-dynamic';
 
 import BlogPostPage from "@/core/presentation/pages/blog-post/blogPostPage"
 import { Loader } from "lucide-react";
