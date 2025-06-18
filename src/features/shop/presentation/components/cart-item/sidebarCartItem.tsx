f03d1a037f7df97c8cd26797b42adf1cc1bd3818
Santiago Velez <santivekez1012@gmail.com>
2025-06-18 17:49:09 -0500

    refactor: Image from nextjs used on cart item

--- src/features/shop/presentation/components/cart-item/sidebarCartItem.tsx
+++ src/features/shop/presentation/components/cart-item/sidebarCartItem.tsx
@@ -1,6 +1,8 @@
 import { CartItem } from '@/features/shop/domain/entities/cartItem';
 import { useCartStore } from '@/features/shop/infrastructure/state/cart.store';
 import React from 'react';
+import Image from 'next/image';
+import { Trash2 } from 'lucide-react';
 
 interface SidebarCartItemProps {
     item: CartItem;
@@ -13,11 +15,7 @@ function SidebarCartItem({ item }: SidebarCartItemProps) {
     return (
         <div key={item.id} className="card card-side bg-base-100 shadow-md">
             <figure className="w-20 h-20 flex-shrink-0 p-2">
-                <img
-                    src={item.imageUrl || "/placeholder.png"}
-                    alt={item.imageAlt || item.title}
-                    className="object-cover rounded-lg w-full h-full"
-                />
+                <Image src={item.imageUrl || "/placeholder.png"} alt={item.imageAlt || item.title} className="object-cover rounded-lg w-full h-full" width={50} height={50} loading='lazy' />
             </figure>
             <div className="card-body p-3 flex flex-row items-center gap-2">
                 <div className="flex-1">
@@ -31,11 +29,7 @@ function SidebarCartItem({ item }: SidebarCartItemProps) {
                     aria-label={`Eliminar ${item.title}`}
                     onClick={() => removeItem(item.id)}
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                            d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+                    <Trash2 className='h-4 w-4' />
                 </button>
             </div>
         </div>
