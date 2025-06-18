5958f0ad26bebcdc5fbd69062262a81ed738ef1e
Santiago Velez <santivekez1012@gmail.com>
2025-06-18 17:40:43 -0500

    refactor: separating cart item component from sidebar cart

---
+++ src/features/shop/presentation/components/cart-item/sidebarCartItem.tsx
@@ -0,0 +1,45 @@
+import { CartItem } from '@/features/shop/domain/entities/cartItem';
+import { useCartStore } from '@/features/shop/infrastructure/state/cart.store';
+import React from 'react';
+
+interface SidebarCartItemProps {
+    item: CartItem;
+}
+
+function SidebarCartItem({ item }: SidebarCartItemProps) {
+
+    const removeItem = useCartStore(state => state.removeItem);
+
+    return (
+        <div key={item.id} className="card card-side bg-base-100 shadow-md">
+            <figure className="w-20 h-20 flex-shrink-0 p-2">
+                <img
+                    src={item.imageUrl || "/placeholder.png"}
+                    alt={item.imageAlt || item.title}
+                    className="object-cover rounded-lg w-full h-full"
+                />
+            </figure>
+            <div className="card-body p-3 flex flex-row items-center gap-2">
+                <div className="flex-1">
+                    <div className="font-semibold text-base">{item.title}</div>
+                    <div className="text-sm text-gray-400">
+                        {item.quantity} x <span className="font-bold">{item.currency} {Number(item.price).toFixed(2)}</span>
+                    </div>
+                </div>
+                <button
+                    className="btn btn-square btn-outline btn-error btn-xs"
+                    aria-label={`Eliminar ${item.title}`}
+                    onClick={() => removeItem(item.id)}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                            d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default SidebarCartItem
--- src/features/shop/presentation/components/cart/cart.tsx
+++ src/features/shop/presentation/components/cart/cart.tsx
@@ -1,10 +1,11 @@
 import { useCartStore } from '@/features/shop/infrastructure/state/cart.store';
 import React from 'react';
+import SidebarCartItem from '../cart-item/sidebarCartItem';
 
 function Cart() {
     const items = useCartStore(state => state.items);
     const total = useCartStore(state => state.total());
-    const removeItem = useCartStore(state => state.removeItem);
+    
 
     return (
         <div className="w-full max-w-sm min-h-full bg-base-200 text-base-content flex flex-col">
@@ -19,34 +20,7 @@ function Cart() {
                 <>
                     <div className="flex flex-col gap-4 overflow-y-auto max-h-[60vh]">
                         {items.map(item => (
-                            <div key={item.id} className="card card-side bg-base-100 shadow-md">
-                                <figure className="w-20 h-20 flex-shrink-0 p-2">
-                                    <img
-                                        src={item.imageUrl || "/placeholder.png"}
-                                        alt={item.imageAlt || item.title}
-                                        className="object-cover rounded-lg w-full h-full"
-                                    />
-                                </figure>
-                                <div className="card-body p-3 flex flex-row items-center gap-2">
-                                    <div className="flex-1">
-                                        <div className="font-semibold text-base">{item.title}</div>
-                                        <div className="text-sm text-gray-400">
-                                            {item.quantity} x <span className="font-bold">{item.currency} {Number(item.price).toFixed(2)}</span>
-                                        </div>
-                                    </div>
-                                    <button
-                                        className="btn btn-square btn-outline btn-error btn-xs"
-                                        aria-label={`Eliminar ${item.title}`}
-                                        onClick={() => removeItem(item.id)}
-                                    >
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none"
-                                            viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                                d="M6 18L18 6M6 6l12 12" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
+                            <SidebarCartItem key={item.id} item={item} />
                         ))}
                     </div>
                     <div className="divider my-2"></div>
