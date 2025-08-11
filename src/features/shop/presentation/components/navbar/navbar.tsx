358138c01d80881aed01cc8c3b7840cef24aa12a
Santiago Velez <santivekez1012@gmail.com>
2025-08-11 09:21:16 -0500

    adding accesibility features to product count

--- src/features/shop/presentation/components/navbar/navbar.tsx
+++ src/features/shop/presentation/components/navbar/navbar.tsx
@@ -10,6 +10,8 @@ function Navbar({ children }: { children: React.ReactNode }) {
 
     const countItems = useCartStore(state => state.items.length);
 
+    const displayCount = countItems > 10 ? '10+' : countItems;
+
     return (
         <div data-theme="dark">
             <div className="drawer">
@@ -46,8 +48,8 @@ function Navbar({ children }: { children: React.ReactNode }) {
                             <label htmlFor="cart-drawer" className="btn btn-ghost btn-circle mr-5 relative">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-shopping-cart-icon lucide-shopping-cart"><circle cx="8" cy="21" r="1" /><circle cx="19" cy="21" r="1" /><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" /></svg>
                                 {countItems > 0 && (
-                                    <span className="absolute -bottom-1 -right-1 bg-primary text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center border-2 border-base-300 animate-bounce">
-                                        {countItems}
+                                    <span className="absolute -bottom-1 -right-1 bg-primary text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center border-2 border-base-300 animate-bounce" aria-label={`${displayCount} items in cart`}>
+                                        {displayCount}
                                     </span>
                                 )}
                             </label>
