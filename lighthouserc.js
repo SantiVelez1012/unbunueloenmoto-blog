9469cc946e6638d2d23e0275263a88e4187c0fa1
Santiago <santiago@Santiagos-MacBook-Pro.local>
2025-12-15 16:59:16 -0500

    adding env variables support to lighthouse report

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
     ci: {
         collect: {
             startServerCommand: 'yarn start',
-            url: ['http://localhost:3000'],
+            url: ['http://localhost:3000/blog', 'http://localhost:3000/shop'],
             startServerReadyTimeout: 20000,
             numberOfRuns: 3,
         },
